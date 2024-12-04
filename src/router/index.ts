3af1256996be03bb441965e8d068d02415e9c325
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2024-12-04 17:28:33 -0300

    Docs.

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,15 +3,27 @@ const allParts = window.location.pathname.split('/');
 const idxBasePart = allParts.findIndex(p => p === BASE);
 const parts = allParts.slice(idxBasePart + 1);
 
+/**
+ * Current application path.
+ */
 export const path = parts.join('/');
 
+/**
+ * Generates a navigable link.
+ */
 export function navLink(path: string): string {
+	if (path.startsWith('/')) {
+		path = path.substring(1);
+	}
 	if (path.endsWith('/')) {
 		path = path.substring(0, path.length - 1);
 	}
-	return '/' + BASE + (path.startsWith('/') ? '' : '/') + path;
+	return '/' + BASE + '/' + path;
 }
 
+/**
+ * Navigates immediately.
+ */
 export function navigate(path: string): void {
 	const link = navLink(path);
 	window.location.href = link;
