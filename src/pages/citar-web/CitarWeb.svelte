f08ff482aff7a4dde54323e6e46872421c689b11
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2024-12-07 14:06:00 -0300

    Isolate state, with derived.

--- src/pages/citar-web/CitarWeb.svelte
+++ src/pages/citar-web/CitarWeb.svelte
@@ -1,26 +1,5 @@
 <script lang="ts">
-	const state = $state({
-		url: '',
-		titulo: '',
-		publicado: '',
-		autorNome: '',
-		autorSobrenome: '',
-		data: '',
-		acessoData: '',
-	});
-
-	const saida = $derived.by(() => {
-		let t = '<ref>{{citar web';
-		if (state.url) t += ' |url=' + state.url;
-		if (state.titulo) t += ' |titulo=' + state.titulo;
-		if (state.publicado) t += ' |publicado=' + state.publicado;
-		if (state.autorNome) t += ' |primeiro=' + state.autorNome;
-		if (state.autorSobrenome) t += ' |ultimo=' + state.autorSobrenome;
-		if (state.data) t += ' |data=' + state.data;
-		if (state.acessoData) t += ' |acessoData=' + state.acessoData;
-		t += '}}</ref>';
-		return t;
-	});
+	import {saida, state} from './store.svelte';
 
 	let txtUrl: HTMLInputElement;
 	$effect(() => {
@@ -47,7 +26,7 @@
 </div>
 
 <div>
-	<textarea class="saida" value={saida} onclick={e => e.currentTarget.select()} readonly></textarea>
+	<textarea class="saida" value={saida()} onclick={e => e.currentTarget.select()} readonly></textarea>
 </div>
 
 <style>
