16c9bf6e06d8455771bd3eccb2ce2d5c64f26034
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2024-12-11 19:44:38 -0300

    Documenting types with examples.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -2,24 +2,45 @@ import type {Component} from 'svelte';
 
 /**
  * A lazy-loaded component.
+ *
+ * @example
+ * const lazyComp = () => import('./home/MyHome.svelte');
  */
 export type LazyComponent = () => Promise<{default: Component}>;
 
 /**
  * URL query parameters.
+ *
+ * @example
+ * const params = {
+ *   name: 'Joe',
+ *   age: 43,
+ * };
  */
 export type QueryParams = Record<string, string | number | null | undefined>;
 
 /**
  * Path and component to an application route, to be defined by the user.
+ *
+ * @example
+ * const route = {
+ *   path: '/page1',
+ *   render: () => import('./page1/Page1.svelte'),
+ * };
  */
 export interface Route {
 	/**
 	 * URL path to match.
+	 *
+	 * @example
+	 * path: '/page1'
 	 */
 	path: string;
 	/**
 	 * Function which lazy loads the component to be rendered.
+	 *
+	 * @example
+	 * render: () => import('./page1/Page1.svelte')
 	 */
 	render: LazyComponent;
 }
@@ -30,6 +51,9 @@ export interface Route {
 export interface RouterConf {
 	/**
 	 * Base URL, as defined in vite.config.ts.
+	 *
+	 * @example
+	 * baseUrl: '/my-application'
 	 */
 	baseUrl?: string;
 	/**
@@ -40,6 +64,9 @@ export interface RouterConf {
 	 * Error 404 route.
 	 *
 	 * If not defined, a simple "404 - Not found" text will be displayed.
+	 *
+	 * @example
+	 * render404: () => import('./Error404.svelte')
 	 */
 	render404?: LazyComponent;
 	/**
