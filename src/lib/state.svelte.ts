bffddba70ffeac4054706a1cf56aa40d2830fbd8
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2024-12-11 16:58:23 -0300

    Isolate utility functions.

--- src/lib/state.svelte.ts
+++ src/lib/state.svelte.ts
@@ -1,4 +1,5 @@
 import type {GetParameters, Route} from './types';
+import {sanitizePath, serializeGetParameters} from './utils';
 
 /**
  * Router state.
@@ -24,42 +25,6 @@ function getCurrentUrlPath(): string {
 	const idxBasePart = allParts.findIndex(p => p === routerState.baseUrl);
 	const parts = allParts.slice(idxBasePart + 1);
 	return parts.join('/');
-}
-
-/**
- * Sanitizes the path, removing "/" prefix and suffix, and removing GET
- * parameters.
- */
-function sanitizePath(path: string): string {
-	let p = path;
-
-	const idxQ = p.indexOf('?');
-	if (idxQ !== -1) p = p.substring(0, idxQ);
-
-	if (p.startsWith('/')) p = p.substring(1);
-	if (p.endsWith('/')) p = p.substring(0, p.length - 1);
-	return p;
-}
-
-/**
- * Serializes an object to a string to be used as GET parameters, including the
- * leading "?".
- */
-function serializeGetParameters(params?: GetParameters): string {
-	const output: Record<string, string> = {};
-	if (params !== undefined) {
-		for (const [key, val] of Object.entries(params)) {
-			if (val === null || val === undefined) {
-				output[key] = '';
-			} else if (typeof val === 'number') {
-				output[key] = val.toString();
-			} else {
-				output[key] = val;
-			}
-		}
-	}
-	const seri = new URLSearchParams(output).toString();
-	return (seri === '') ? '' : ('?' + seri);
 }
 
 /**
---
+++ src/lib/utils.ts
@@ -0,0 +1,37 @@
+import type {GetParameters} from './types';
+
+/**
+ * Sanitizes the path, removing "/" prefix and suffix, and removing GET
+ * parameters.
+ */
+export function sanitizePath(path: string): string {
+	let p = path;
+
+	const idxQ = p.indexOf('?');
+	if (idxQ !== -1) p = p.substring(0, idxQ);
+
+	if (p.startsWith('/')) p = p.substring(1);
+	if (p.endsWith('/')) p = p.substring(0, p.length - 1);
+	return p;
+}
+
+/**
+ * Serializes an object to a string to be used as GET parameters, including the
+ * leading "?".
+ */
+export function serializeGetParameters(params?: GetParameters): string {
+	const output: Record<string, string> = {};
+	if (params !== undefined) {
+		for (const [key, val] of Object.entries(params)) {
+			if (val === null || val === undefined) {
+				output[key] = '';
+			} else if (typeof val === 'number') {
+				output[key] = val.toString();
+			} else {
+				output[key] = val;
+			}
+		}
+	}
+	const seri = new URLSearchParams(output).toString();
+	return (seri === '') ? '' : ('?' + seri);
+}
